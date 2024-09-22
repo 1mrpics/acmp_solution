f5a8ec9ab54dc680fdf27625c848e91e429528cb
Michalchuk Ivan <93178265+1mrpics@users.noreply.github.com>
2024-09-22 16:43:56 +0700

    Update 2.py

--- 2/2.py
+++ 2/2.py
@@ -1,1 +1,14 @@
+N = int(input())
+summa = 0
 
+if N > 0:
+    for i in range(N+1):
+        summa += i
+    print(summa)
+elif N < 0:
+    for i in range(N, 2):
+        summa += i
+    print(summa)
+else:
+    print(1)
+  
